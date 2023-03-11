590f295c7ffba3bd6c0ba95b5f9a18fb70368aaf
Daria <118595398+dariavarlamovaa@users.noreply.github.com>
2023-03-11 20:16:01 +0300

    edited

--- lesson10.py
+++ lesson10.py
@@ -1,54 +1,54 @@
 # 'tic-tac-toe'
-board = [['#', '#', '#'],
-         ['#', '#', '#'],
-         ['#', '#', '#']]
-turn = 1
+# board = [['#', '#', '#'],
+#          ['#', '#', '#'],
+#          ['#', '#', '#']]
+# turn = 1
 
 
-def check_win(board, symbol):
-    if ((board[0][0] == board[0][1] == board[0][2] == symbol) or
-            (board[1][0] == board[1][1] == board[1][2] == symbol) or
-            (board[2][0] == board[2][1] == board[2][2] == symbol) or
-            (board[0][0] == board[1][1] == board[2][2] == symbol) or
-            (board[0][2] == board[1][1] == board[2][0] == symbol) or
-            (board[0][0] == board[1][0] == board[2][0] == symbol) or
-            (board[0][1] == board[1][1] == board[2][1] == symbol) or
-            (board[0][2] == board[1][2] == board[2][2] == symbol)):
-        return True
-    return False
+# def check_win(board, symbol):
+#     if ((board[0][0] == board[0][1] == board[0][2] == symbol) or
+#             (board[1][0] == board[1][1] == board[1][2] == symbol) or
+#             (board[2][0] == board[2][1] == board[2][2] == symbol) or
+#             (board[0][0] == board[1][1] == board[2][2] == symbol) or
+#             (board[0][2] == board[1][1] == board[2][0] == symbol) or
+#             (board[0][0] == board[1][0] == board[2][0] == symbol) or
+#             (board[0][1] == board[1][1] == board[2][1] == symbol) or
+#             (board[0][2] == board[1][2] == board[2][2] == symbol)):
+#         return True
+#     return False
 
 
-def make_turn(board, symbol):
-    while True:
-        try:
-            row = int(input('введите номер ряда (1-3): '))
-            col = int(input('введите номер столбца (1-3): '))
-            if ((row in [1, 2, 3] and col in [1, 2, 3]) and
-                    board[row - 1][col - 1] == '#'):
-                break
-            else:
-                print('ошибка! неверный ввод.')
-        except ValueError:
-            pass
-    board[row - 1][col - 1] = symbol
+# def make_turn(board, symbol):
+#     while True:
+#         try:
+#             row = int(input('введите номер ряда (1-3): '))
+#             col = int(input('введите номер столбца (1-3): '))
+#             if ((row in [1, 2, 3] and col in [1, 2, 3]) and
+#                     board[row - 1][col - 1] == '#'):
+#                 break
+#             else:
+#                 print('ошибка! неверный ввод.')
+#         except ValueError:
+#             pass
+#     board[row - 1][col - 1] = symbol
 
 
-def tic_tac_toe(board, turn):
-    if turn > 9:
-        print('ничья')
-        return
-    symbol = 'X' if turn % 2 == 1 else 'О'
-    print(f'ходят {symbol}. ход: {turn}')
-    [print(*row, sep='\t') for row in board]
-    make_turn(board, symbol)
-    if check_win(board, symbol):
-        print(f'победили {symbol}!')
-        [print(*row, sep='\t') for row in board]
-    else:
-        tic_tac_toe(board, turn + 1)
+# def tic_tac_toe(board, turn):
+#     if turn > 9:
+#         print('ничья')
+#         return
+#     symbol = 'X' if turn % 2 == 1 else 'О'
+#     print(f'ходят {symbol}. ход: {turn}')
+#     [print(*row, sep='\t') for row in board]
+#     make_turn(board, symbol)
+#     if check_win(board, symbol):
+#         print(f'победили {symbol}!')
+#         [print(*row, sep='\t') for row in board]
+#     else:
+#         tic_tac_toe(board, turn + 1)
 
 
-tic_tac_toe(board, 1)
+# tic_tac_toe(board, 1)
 
 
 # import random
